08ea806098a596f088c944518ca22ec79dfa137a
Bingo <61998928+beenbin@users.noreply.github.com>
2020-07-09 03:07:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,9 @@
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package.git' feeds.conf.default
+#sed -i '$a src-git beenbin https://github.com/beenbin/passwall652.git' feeds.conf.default
